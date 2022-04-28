fda57d01cb64b345ffc66c79d998d50198512daa
guilherme pires <guilhermedasilvapires1996@gmail.com>
2022-04-28 12:59:44 -0300

    Aprendendo a usar Biblioteca Pytest-Mock

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,5 +1,4 @@
 from unittest.mock import Mock
-
 import pytest
 
 from libpythonpro import github_api
@@ -8,13 +7,13 @@ from libpythonpro import github_api
 @pytest.fixture
 def avatar_url(mocker):
     resp_mock = Mock()
-    url = 'https://avatars3.githubusercontent.com/u/402714?v=4'
+    url = 'https://avatars3.githubusercontent.com/u/3457115?v=4'
     resp_mock.json.return_value = {
         'login': 'renzo', 'id': 402714,
         'avatar_url': url,
     }
     get_mock = mocker.patch('libpythonpro.github_api.requests.get')
-    get_mock.return_value = resp_mock
+    get_mock.return_value=resp_mock
     return url
 
 
@@ -23,6 +22,7 @@ def test_buscar_avatar(avatar_url):
     assert avatar_url == url
 
 
+
 def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('renzon')
-    assert 'https://avatars3.githubusercontent.com/u/3457115?v=4' == url
+    assert 'https://avatars.githubusercontent.com/u/3457115?v=4' == url
--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,17 +1,22 @@
 from libpythonpro.spam.modelos import Usuario
 
 
-def test_salvar_usuario(sessao):
+def test_salvar_usuario(conexao, sessao):
+    sessao = conexao.gerar_sessao()
     usuario = Usuario(nome='Renzo', email='renzo@python.pro.br')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
+    sessao.roll_back()
 
-
-def test_listar_usuarios(sessao):
+def test_listar_usuarios(conexao, sessao):
+    sessao = conexao.gerar_sessao()
     usuarios = [
         Usuario(nome='Renzo', email='renzo@python.pro.br'),
-        Usuario(nome='Luciano', email='renzo@python.pro.br')
+        Usuario(nome='Luciano', email='renzo@python.pro.br'),
+        Usuario(nome='Guilherme', email='guilhermedasilvapires1996@gmail.com')
     ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
+    sessao.roll_back()
+
